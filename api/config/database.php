969a9ab4dd3af8c251a6e3eaf65e37eef73b220e
Kanon <gokagokaigokai@gmail.com>
2022-04-23 02:14:50 -0400

    Hosted to DIRO

--- api/config/database.php
+++ api/config/database.php
@@ -3,14 +3,14 @@
 class Database{
  
     // specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "projet3";
-    private $username = "root";
-    private $password = "";
-    // private $host = "www-ens.iro.umontreal.ca";
-    // private $db_name = "pift3225_web_api_db";
-    // private $username = "pift3225";
-    // private $password = "225pifti";
+    // private $host = "localhost";
+    // private $db_name = "projet3";
+    // private $username = "root";
+    // private $password = "";
+    private $host = "www-ens.iro.umontreal.ca";
+    private $db_name = "oudanons_web";
+    private $username = "oudanons";
+    private $password = "Ecole1234!!";
     public $conn;
 
     // get the database connection
